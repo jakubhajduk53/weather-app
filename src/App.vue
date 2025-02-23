745cef31fa3b8b78efcb85b78cf61c3ed66d79b0
Jakub Hajduk <jakubhajduk53@gmail.com>
2025-02-23 17:34:51 +0100

    feat: setup api

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useWeatherStore } from "./store/store";
+import { onMounted } from "vue";
+
+const weatherStore = useWeatherStore();
+
+onMounted(async () => {
+  await weatherStore.fetch("warsaw");
+});
+</script>
 
 <template>
-  <div></div>
+  <div class="flex flex-col items-center">
+    <div>{{ weatherStore.weatherData.name }}</div>
+    <div>{{ weatherStore.weatherData.main.temp }}</div>
+  </div>
 </template>
 
 <style scoped></style>
